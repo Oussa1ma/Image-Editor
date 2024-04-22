e7fa55ef2602da54ab943904401b279ac96906d3
Oussa1ma <oussama.elmortaji.2013@gmail.com>
2024-04-22 19:02:50 +0100

    Initialize DOM elements for image editor interface

---
+++ main.ts
@@ -0,0 +1,12 @@
+const contrast = document.getElementById("contrast");
+const brightness = document.getElementById("brightness");
+const sepia = document.getElementById("sepia");
+const grayscale = document.getElementById("grayscale");
+const blurInput = document.getElementById("blur");
+
+const upload = document.getElementById("upload");
+const save = document.getElementById("save");
+const img = document.getElementById("img");
+
+const undo = document.querySelector("span");
+const imgBox = document.querySelector(".img-box");
