b2e584aff76717fe864b27076d7c342a8295503b
Oussa1ma <oussama.elmortaji.2013@gmail.com>
2024-04-22 19:20:15 +0100

    Implement upload image functionality

--- main.ts
+++ main.ts
@@ -1,12 +1,32 @@
-const contrast = document.getElementById("contrast");
-const brightness = document.getElementById("brightness");
-const sepia = document.getElementById("sepia");
-const grayscale = document.getElementById("grayscale");
-const blurInput = document.getElementById("blur");
+const contrast = document.getElementById("contrast") as HTMLInputElement;
+const brightness = document.getElementById("brightness") as HTMLInputElement;
+const sepia = document.getElementById("sepia") as HTMLInputElement;
+const grayscale = document.getElementById("grayscale") as HTMLInputElement;
+const blurInput = document.getElementById("blur") as HTMLInputElement;
 
-const upload = document.getElementById("upload");
-const save = document.getElementById("save");
-const img = document.getElementById("img");
+const upload = document.getElementById("upload") as HTMLInputElement;
+const save = document.getElementById("save") as HTMLAnchorElement;
+const img = document.getElementById("img") as HTMLImageElement;
 
-const undo = document.querySelector("span");
-const imgBox = document.querySelector(".img-box");
+const undo = document.querySelector("span") as HTMLSpanElement;
+const imgBox = document.querySelector(".img-box") as HTMLDivElement;
+
+//hide the image box and buttons when loading the page
+window.onload = () => {
+  save.style.display = "none";
+  imgBox.style.display = "none";
+  undo.style.display = "none";
+};
+
+//show the image box and buttons when choosing the image
+upload.onchange = () => {
+  save.style.display = "block";
+  imgBox.style.display = "block";
+  undo.style.display = "block";
+  //use fileReader class to read the uploaded omage
+  const file = new FileReader();
+  file.readAsDataURL(upload.files![0]); //first file in the array (selected image)
+  file.onload = () => {
+    img.src = file.result as string;
+  };
+};
